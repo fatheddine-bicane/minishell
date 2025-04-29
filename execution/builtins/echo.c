b055b77e481e13bbfe5fdf6d3eb5be21d580bab0
fatheddine-bicane <fatheddine04@gmail.com>
2025-04-29 21:07:55 +0100

    fix: expander edge cases

--- execution/builtins/echo.c
+++ execution/builtins/echo.c
@@ -12,6 +12,27 @@
 
 #include "../../minishel.h"
 
+void	ft_handl_n(char **arr, int *j, bool *new_line) // INFO: handle -n flag
+{
+	int	i;
+
+	i = 0;
+	while (arr[*j][0] == '-' && arr[*j])
+	{
+		i = 1;
+		while (arr[*j][i] == 'n' && arr[*j][i])
+			i++;
+		if (arr[*j][i])
+		{
+			break;
+			j--;
+		}
+		else
+			(*new_line) = false;
+		j++;
+	}
+}
+
 void	ft_print_var(int len, char *variable, t_list *my_envp)
 {
 	while (my_envp)
@@ -23,43 +44,58 @@ void	ft_print_var(int len, char *variable, t_list *my_envp)
 		}
 		my_envp = my_envp->next;
 	}
+	if (1 == ft_strlen(variable))
+		printf("$");
+}
+
+void	ft_expand_var(char *str, int *j, t_list *my_envp)// INFO: expand the var from envp
+{
+	int	i;
+
+	if (1 == ft_strlen(str))
+	{
+		printf("$");
+		(*j)++;
+		return ;
+	}
+	i = 0;
+	while ('=' != str[i] && str[i])
+		i++;
+	ft_print_var(i - 1, str + 1, my_envp);
+	printf("%s", str + i);
+	(*j)++;
+}
+
+void	ft_handl_ds(char *str)
+{
+	if (str[1] != '_' && !ft_isalpha(str[1]))
+	{
+		// TODO: handle cases where the argg start with $
+	}
 }
 
 void	ft_echo(char *rl, t_list *my_envp)
 {
 	char **arr;
-	int	i;
-	int	j = 1;
-	bool	new_line = true;
+	/*int	i;*/
+	int	j;
+	bool	new_line;
 
 	arr = ft_split(rl, 32);
+	j = 1;
+	new_line = true;
 
-	while (arr[j] && arr[j][0] == '-')
-	{
-		i = 1;
-		while (arr[j][i] == 'n' && arr[j][i])
-			i++;
-		if (arr[j][i])
-		{
-			break;
-			j--;
-		}
-		else
-			new_line = false;
-		j++;
-	}
-
-	if (arr[j][0] == '$')
-	{
-		i = 0;
-		while (arr[j][i] != '=' && arr[j][i])
-			i++;
-		ft_print_var(i - 1, arr[j] + 1, my_envp);
-		j++;
-	}
+	ft_handl_n(arr, &j, &new_line);
 
 	while (arr[j])
 	{
+		if ('$' == arr[j][0])
+		{
+			ft_expand_var(arr[j], &j, my_envp);
+			if (arr[j + 1])
+				printf(" ");
+			continue ;
+		}
 		printf("%s", arr[j]);
 		if (arr[j + 1])
 			printf(" ");
