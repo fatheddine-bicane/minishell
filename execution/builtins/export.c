7bbda605e6ed32bf8533c52deee7952358f2e911
fatheddine-bicane <fatheddine04@gmail.com>
2025-04-30 18:58:12 +0100

    feat: export implemented

--- execution/builtins/export.c
+++ execution/builtins/export.c
@@ -12,6 +12,72 @@
 
 #include "../../minishel.h"
 
+bool	ft_envpcmp(char *variable, char *envp_l)
+{
+	int (i) = 0;
+	while (envp_l[i] != '=')
+		i++;
+	char (str[i]);
+	i = 0;
+	while (envp_l[i] != '=')
+		str[i] = envp_l[i];
+	str[i] = '\0';
+	if (!ft_strncmp(variable, str, ft_strlen(variable)))
+		return (true);
+	else
+		return (false);
+}
+
+bool	ft_variable_exist(t_list *my_envp, char *variable)
+{
+	while (my_envp)
+	{
+		if (ft_envpcmp(variable, my_envp->content))
+			return (true);
+		my_envp = my_envp->next;
+	}
+	return (false);
+}
+
+static char	*ft_find_var(char *variable, t_list *my_vars)
+{
+	int	i;
+
+	while (my_vars)
+	{
+		i = 0;
+		while (variable[i] != '=' && variable[i])
+			i++;
+		char	str[i];
+		i = 0;
+		while (variable[i] != '=' && variable[i])
+		{
+			str[i] = variable[i];
+			i++;
+		}
+		str[i] = '\0';
+		if (!ft_strncmp(variable, str, ft_strlen(variable)))
+		{
+			return ((char *)my_vars->content);
+		}
+		my_vars = my_vars->next;
+	}
+	return (NULL);
+}
+
+void	ft_export_util(t_list **my_envp, char *variable, t_list *my_vars)
+{
+	if (!ft_variable_exist((*my_envp), variable))
+	{
+		char (*var_to_export) = ft_find_var(variable, my_vars);
+		if (!var_to_export)
+		{
+			return ;
+		}
+		ft_lstadd_back(my_envp, ft_lstnew(var_to_export));
+	}
+}
+
 void	ft_export(char **envp)
 {
 	(void)envp;
