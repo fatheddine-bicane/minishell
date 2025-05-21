9031e60a1d9027d25ea6c8a77fd86661c41fbaef
fatheddine-bicane <fatheddine04@gmail.com>
2025-05-21 22:00:01 +0100

    feat: signals (wip)

---
+++ execution/signals/signals.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbicane <fbicane@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 10:18:48 by fbicane           #+#    #+#             */
+/*   Updated: 2025/05/21 10:33:46 by fbicane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishel.h"
+
+//to handle ctrl / maybe use sigemptyset() and sigaddset() to add the new signal
+
+void	ft_signal(void)
+{
+	sigset_t	*test;
+
+	
+}
--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbicane <fbicane@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/06 21:54:56 by fbicane           #+#    #+#             */
-/*   Updated: 2025/05/14 22:55:46 by fbicane          ###   ########.fr       */
+/*   Created: 2025/05/20 14:21:00 by fbicane           #+#    #+#             */
+/*   Updated: 2025/05/20 14:34:15 by fbicane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,27 @@
 /*	return data;*/
 /*}*/
 
+void	sigint_handler(int signal)
+{
+	if (SIGINT == signal)
+	{
+		g_signal_flag = SIGINT;
+		rl_on_new_line();
+		/*rl_replace_line("", 0);*/
+		rl_redisplay();
+	}
+}
+
+void	ft_setup_signals(void)
+{
+	struct sigaction signals;
+
+	signals.sa_handler = sigint_handler;
+	sigemptyset(&signals.sa_mask);
+	signals.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &signals, NULL);
+}
+
 int main(int argc, char **argv, char **envp)
 {
 	t_list *my_envp;
@@ -28,11 +49,18 @@ int main(int argc, char **argv, char **envp)
 	(void)argv;
 
 	my_envp = ft_set_env(envp);
+	ft_setup_signals();
 	while (1)
 	{
+		g_signal_flag = 0;
 		char (*rl) = readline("====> ");
+		if (SIGINT == g_signal_flag)
+		{
+			free (rl);
+			continue;
+		}
 		/*rl = readline("╭─ minishell \n╰─> ");*/
-		//BUG: if an empty line is set to rl there is segfault
+		// BUG: if an empty line is set to rl there is segfault
 
 		// INFO: to test pipes
 		/*ft_pipex(ft_split(rl, '|'), &my_envp);*/
