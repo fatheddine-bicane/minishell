e01b53562a49daacd04e0802511e348a44b54cf4
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-05-29 15:17:46 +0100

    feat: keep split str from str builder

--- parser/ast/exec.c
+++ parser/ast/exec.c
@@ -87,17 +87,15 @@ t_cmd	*parse_cmd(t_token **token)
 	cmd = parse_redirect(token);
 	while (match_token(token, 4, T_WORD, T_VAR, T_STR_SINGLE, T_STR_DOUBLE))
 	{
-		if (sb_len(sb) > 0 && !sb_append_char(sb, ' '))
-			return (sb_free(sb), ast_free(cmd), NULL);
 		if (!sb_append_str(sb, (*token)->prev->lexeme, 0))
 			return (sb_free(sb), ast_free(cmd), NULL);
 		cmd = append_cmd(cmd, parse_redirect(token));
 	}
 	if (cmd == NULL && sb_len(sb) > 0)
-		return (cmd_exec_init(sb_split(sb, ' ')));
+		return (cmd_exec_init(sb_build(sb)));
 	if (sb_len(sb) > 0)
 	{
-		tmp = cmd_exec_init(sb_split(sb, ' '));
+		tmp = cmd_exec_init(sb_build(sb));
 		if (tmp == NULL)
 			return (ast_free(cmd), NULL);
 		append_cmd(cmd, tmp);
