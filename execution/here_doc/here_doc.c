98d7f83f8a3badc94fc107aca45a85c68470627b
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-06-13 19:32:47 +0100

    fix: free appended heredoc line

--- execution/here_doc/here_doc.c
+++ execution/here_doc/here_doc.c
@@ -64,6 +64,7 @@ static char *ft_creat_input(char *limiter, t_shell *shell)
 		}
 		sb_append_str(sb, str, 0);
 		sb_append_char(sb, '\n');
+		free(str);
 	}
 	return (free(limiter), sb_build_str(sb));
 }
