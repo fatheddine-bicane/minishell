cb6369c4cf2df3126a2e50ffcd6dff8563aeb25e
fatheddine-bicane <fatheddine04@gmail.com>
2025-06-16 19:12:35 +0100

    fix: norminette signals

--- execution/signals/sognals_heredoc.c
+++ execution/signals/sognals_heredoc.c
@@ -1,40 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   signals.c                                          :+:      :+:    :+:   */
+/*   sognals_heredoc.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbicane <fbicane@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/21 10:18:48 by fbicane           #+#    #+#             */
-/*   Updated: 2025/06/16 19:07:00 by fbicane          ###   ########.fr       */
+/*   Created: 2025/06/16 19:06:45 by fbicane           #+#    #+#             */
+/*   Updated: 2025/06/16 19:06:56 by fbicane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishel.h"
 
-void	sigint_handler(int signal)
+void	sigquit_heredoc_handler(int signal)
 {
 	(void) signal;
-	g_signal_flag = SIGINT;
 	write(STDOUT_FILENO, "\n", 1);
-	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
 }
 
-void	setup_signals(void)
-{
-	struct sigaction	signals;
-
-	sigemptyset(&signals.sa_mask);
-	signals.sa_flags = SA_RESTART;
-	signals.sa_handler = sigint_handler;
-	sigaction(SIGINT, &signals, NULL);
-	signals.sa_handler = SIG_IGN;
-	sigaction(SIGQUIT, &signals, NULL);
-}
-
-void	setup_signals_child(void)
+void	setup_signals_heredoc(void)
 {
 	struct sigaction	signals;
 
@@ -42,18 +28,6 @@ void	setup_signals_child(void)
 	signals.sa_flags = SA_RESTART;
 	signals.sa_handler = SIG_DFL;
 	sigaction(SIGINT, &signals, NULL);
-	signals.sa_handler = SIG_DFL;
-	sigaction(SIGQUIT, &signals, NULL);
-}
-
-void	ignore_signals_parrent(void)
-{
-	struct sigaction	signals;
-
-	sigemptyset(&signals.sa_mask);
-	signals.sa_flags = SA_RESTART;
-	signals.sa_handler = SIG_IGN;
-	sigaction(SIGINT, &signals, NULL);
-	signals.sa_handler = SIG_IGN;
+	signals.sa_handler = sigquit_heredoc_handler;
 	sigaction(SIGQUIT, &signals, NULL);
 }
