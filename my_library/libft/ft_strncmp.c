088317bf5bde5d9750c5e2998bc18e8a63bdcc16
fatheddine-bicane <fatheddine04@gmail.com>
2025-06-16 18:45:22 +0100

    fix: ft_strncmp bug (n = 0)

--- my_library/libft/ft_strncmp.c
+++ my_library/libft/ft_strncmp.c
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	if (n == 0)
-		return (0);
+		return ((unsigned char)s1[0] - (unsigned char)s2[0]);
 	if (!s1 && !s2)
 		return (0);
 	i = 0;
